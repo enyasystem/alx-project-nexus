b0f5d7c8d8429abef225f7db3e7eef0a27ea03a2
enyasystem <enyaelvis@gmail.com>
2025-09-26 14:01:10 +0100

    Remove generated GH run artifacts and update .gitignore

--- make_patch.py
+++
@@ -1,7 +0,0 @@
-import json
-p=json.load(open('main_protection_backup.json','r',encoding='utf8'))
-if 'required_pull_request_reviews' not in p:
-    p['required_pull_request_reviews']={}
-p['required_pull_request_reviews']['required_approving_review_count']=0
-json.dump(p, open('main_protection_patch.json','w',encoding='utf8'))
-print('Wrote main_protection_patch.json')
